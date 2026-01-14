926d4e453f19a6efc6c5a3b8fa1b5e43f7fc5bbd
mayankbhagat <bhagatmayank2005@gmail.com>
2026-01-14 23:07:19 +0530

    new changes

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -5,7 +5,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 load_dotenv()
 
-llm_model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+llm_model = ChatGoogleGenerativeAI(model="models/gemini-1.5-flash-latest")
 
 # MODIFIED: retrieve_docs now accepts 'db_to_use'
 def retrieve_docs(query, db_to_use):
